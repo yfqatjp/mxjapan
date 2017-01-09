0b4f3ba8eaea3e90359bedaec9863432ef7b4bf2
test <yuefuquan@gmail.com>
2017-01-09 21:25:35 +0900

    医疗程序更新

---
+++ hm/head.php
@@ -0,0 +1,20 @@
+<div class="sehun"></div>
+<header id="fh5co-header" role="banner">
+    <div class="container">
+        <div class="header-inner">
+            <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+            <nav role="navigation" class="navshow">
+                <ul>
+                    <?php
+                    $rs = $pdo->query("SELECT * FROM `pm_page` WHERE `lang` = '2' AND `main` = 1 AND `id_parent` IS NULL ORDER BY rank");
+                    while ($row = $rs->fetch()) {
+                        ?>
+                        <li<?php if ($navid == $row['id']) { ?> class="navs"<?php } ?>><a
+                                href="<?php echo $row['url'] ?>"><?php echo $row['name'] ?></a></li>
+                    <?php } ?>
+                    <li class="cta"><a href="signin.html">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
