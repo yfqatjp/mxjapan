656cf6e7e8f3ab5182d5f6f9b48d53e335058134
test <yuefuquan@gmail.com>
2017-01-30 17:07:55 +0900

    支付修改

--- pay/paypal/alipayto.php
+++ pay/paypal/alipayto.php
@@ -5,14 +5,20 @@ $o = @$_GET['o'];
 $rs = $pdo->query("SELECT * FROM pm_gwc WHERE onum LIKE  '" . $o . "' AND uid = " . $_SESSION['userid']);
 $row = $rs->fetch();
 ?>
+<!-- 
 <form action="https://www.paypal.com/hk/cgi-bin/webscr" name="payForm" id="payForm" method="post">
+ -->
+ 
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" name="payForm" id="payForm" method="post">
     <input type="hidden" name="cmd" value="_xclick">
     <input type="hidden" name="charset" value="utf-8">
     <input type="hidden" name="item_number" value="<?php echo $o ?>">
-    <input type="hidden" name="business" value="">
+    <input type="hidden" name="business" value="yuefuquan-facilitator@gmail.com">
     <input type="hidden" name="item_name" value="订单ID:<?php echo $o ?>">
-    <input type="hidden" name="currency_code" value="RMB">
-    <input type="hidden" name="amount" value="<?php echo $row['price'] ?>">
+    <input type="hidden" name="currency_code" value="JPY">
+   <!-- <input type="hidden" name="amount" value="<?php echo $row['price'] ?>"> --> 
+    <input type="hidden" name="amount" value="2">
+    
     <input type="hidden" name="shipping" value="0">
     <input type="hidden" name="cancel_return" value="http://<?php echo $_SERVER['HTTP_HOST'] ?>">
     <input type="hidden" name="return" value="http://<?php echo $_SERVER['HTTP_HOST'] ?>/user/jddd.html">
