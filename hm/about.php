1fdcb1e26b2352750cf46201e0bd26854a8db21e
Daniel Dan <erlong.duan@gmail.com>
2017-02-25 10:12:38 +0900

    修改

--- hm/about.php
+++ hm/about.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<?php require_once 'coon.php';
+$navid = 2;
+$rs = $pdo->query("SELECT * FROM `pm_page` WHERE `lang` = '2' AND id = " . $navid);
+$row = $rs->fetch();
+
+$rs2 = $pdo->query("SELECT * FROM pm_article_file WHERE checked = 1 AND lang = 2 AND type = 'image' AND file != '' ORDER BY rank LIMIT 1");
+$row2 = $rs2->fetch();
+?><!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -6,74 +13,14 @@
 <html class="no-js">
 <!--<![endif]-->
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>美溪民宿</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
-<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-
-<!-- Facebook and Twitter integration -->
-<meta property="og:title" content=""/>
-<meta property="og:image" content=""/>
-<meta property="og:url" content=""/>
-<meta property="og:site_name" content=""/>
-<meta property="og:description" content=""/>
-<meta name="twitter:title" content="" />
-<meta name="twitter:image" content="" />
-<meta name="twitter:url" content="" />
-<meta name="twitter:card" content="" />
-
-<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-<link rel="shortcut icon" href="favicon.ico">
-<link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,700" rel="stylesheet">
-
-<!-- Animate.css -->
-<link rel="stylesheet" href="css/animate.css">
-<!-- Icomoon Icon Fonts-->
-<link rel="stylesheet" href="css/icomoon.css">
-<!-- Bootstrap  -->
-<link rel="stylesheet" href="css/bootstrap.css">
-<!-- Flexslider  -->
-<link rel="stylesheet" href="css/flexslider.css">
-<!-- Owl Carousel  -->
-<link rel="stylesheet" href="css/owl.carousel.min.css">
-<link rel="stylesheet" href="css/owl.theme.default.min.css">
-<!-- Theme style  -->
-<link rel="stylesheet" href="css/style.css">
-
-<!-- Modernizr JS -->
-<script src="js/modernizr-2.6.2.min.js"></script>
-<!-- FOR IE9 below -->
-<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-<script src="js/jquery.min.js"></script>
-<style>
-body{ background:#f7f7f7;}
-</style>
+    <?php require_once 'top.php'; ?>
+    <style>
+        body{ background:#f7f7f7;}
+    </style>
 </head>
 <body>
 <div class="sehun"></div>
-<header id="fh5co-header" role="banner">
-  <div class="container">
-    <div class="header-inner">
-      <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
-      <nav role="navigation" class="navshow">
-        <ul>
-          <li><a href="index.html">首页</a></li>
-          <li><a href="guide.html">车导服务</a></li>
-          <li><a href="list2.html">民宿</a></li>
-          <li><a href="medical.html">医疗</a></li>
-          <li><a href="gallery.html">旅游图库</a></li>
-          <li><a href="realestate.html">不动产服务</a></li>
-          <li class="navs"><a href="about.html">关于我们</a></li>
-          <li class="cta"><a href="signin.html">登录</a></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-</header>
+<?php require_once 'head.php'; ?>
 <div class="container"> </div>
 <aside id="fh5co-hero" class="js-fullheight">
   <div class="flexslider js-fullheight">
